26f085cb1780c64e0e8a30886c3a4fc94789aa90
sangram <balsangram1@gmail.com>
2025-07-03 21:19:28 +0530

    update

--- art-of-living-1b75a-firebase-adminsdk-fbsvc-a08df784cc.js
+++ art-of-living-1b75a-firebase-adminsdk-fbsvc-a08df784cc.js
@@ -1,15 +1,32 @@
+// export const serviceAccount = {
+//   type: "service_account",
+//   project_id: "aolic-a3c42",
+//   private_key_id: "f517b1fe88c9002367fa684ddbe25028310b107a",
+//   private_key:
+//     "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNvp9sbJIZLfeu\n6ahj0MQvXecdclGFkAzbS+e1aMcRpGXTwuElVUL0Qm0aKexO5HdYij4pjL2AiIxM\na0hr3ng9mfj+Hf6PewMp7csFq8peRjtHWj0R4aYCaYuK1y2HP0v4OHmTxVgjl4eq\nyM7Ckzl+3bdxG8vFbYUsQwaCYc75bZ0Wv2I05Qm0CE/n8rGSAMkJ1JJnGq6S9a6v\nlz1xmmFOR8Yt7knQFddzumoP8/IAsWBs/9WyKE6FiknRGuxZkwb/G0mmmUwyYKgP\nUhsaz3/VB0H0y3js+9JnBQgyFu6sXPOFKDrDGDcSgPyQJabiOIL0Uxju+Gel070l\n8A/IkJWlAgMBAAECggEAL3rSJ59Wh+EHy6G4bnB7FcNB1pXPIld/4AvASSkI55Vc\numW6t3bZoKzDqMoL8pjewdwFRGlqQgNA/VG0wM/yR6UExP//AvWMYVyzAXgHl4H1\npB27AilkuiHJRh0qMI/25C2EQQnFsVECyfLVZCm9lv1rm/qiJ8mCdhhxpB0WOpEb\nESyrVn7b4JHaQQUv1raj2q25ChF2s1XfDVGkoHvZezOQVfXeeESso9oHOv+uDv/q\nKgUfHoEus22zYTsTYaAN63BirXvUR9AH8ElprxnFYJDAxB0J04TbiVKj8TlcrdSX\nKw2zfeGVltpKTF6fXwqLvCs1V05XgX47EmRgQQGzcQKBgQD0WG7a1masHUtdI9FJ\nHLZob1glHCSy7mnvTB4BQQG7xDbu/AWmbTpykN8e/zAUavALONnHgWe2lGaJPHhO\nBoR36UuFFCD2YSXMTgyGG36RLBDrGxu/nrotFO+XrZeT8viOKKxscPt5XY+6kbzW\nZ5DK7CI8z+eWOg1iP+3MihnBZwKBgQDXjttAbSYabuWo/BDffRpDD5emX5GyKsyS\nCeKwisYcyY6l4OTW3/5zbyr/9goPTeJ9Ujj+fR7jj26FWuhHTyOw8n9L0ap56UK3\ndWrJmNEpQTL7bmh2mtrzSDq5ggm3FxO9dDvqSnFzj29PBCYgnbHlSDEa6S0jwWUM\n9QyTuWMNEwKBgFp54Fph51S16DEN0TH8UsKq7/2VxTUIOOulyLp4IrcvOZQWd9Uf\nPvQrFqM4pMn6qvXM0D8ieI/PtYP9gyKJIUXxechGIsp4QHG9OL/zsFL6+SzFNeon\nURLeXLVSTv02i7i/8NCaqVNQRKc1zbGqWZ/5/dd8uGkVB92ohFonjM/jAoGALH3h\nBVhCEByprIpylR6GSCc0vvNk6ChrVw1RS+0o3RJBLj3QHgXXyE2+y1wmUWhbAlDx\nUJ3UvVWCk2XjIazVChXTbnvVOxqJWcKTiw7OxTwe4MS2LRzjammTLW0IwlCyE12l\nbEN3TUniyKVrlTnXWSLgd9R/K60kRVtjAC64oh8CgYEA8RJuts1HrVo/qSqzoK0Q\nFuA5N5Hg3SL/mygS8kBVMtnecUtRpY6HFwY0Wg/tQRwzixSBVINLW9CDydcxUy/X\nOv+y7e56VPLJsk76VkzChTxPtNLiAxTqItNBbt+Si3w59NvfkkJMNalinN8S1gO5\ncaYcdNboL2jM2iv1U6eQwiM=\n-----END PRIVATE KEY-----\n",
+//   client_email: "firebase-adminsdk-fbsvc@aolic-a3c42.iam.gserviceaccount.com",
+//   client_id: "101596503546946949609",
+//   auth_uri: "https://accounts.google.com/o/oauth2/auth",
+//   token_uri: "https://oauth2.googleapis.com/token",
+//   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+//   client_x509_cert_url:
+//     "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aolic-a3c42.iam.gserviceaccount.com",
+//   universe_domain: "googleapis.com",
+// };
+
+
 export const serviceAccount = {
-  type: "service_account",
-  project_id: "aolic-a3c42",
-  private_key_id: "f517b1fe88c9002367fa684ddbe25028310b107a",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNvp9sbJIZLfeu\n6ahj0MQvXecdclGFkAzbS+e1aMcRpGXTwuElVUL0Qm0aKexO5HdYij4pjL2AiIxM\na0hr3ng9mfj+Hf6PewMp7csFq8peRjtHWj0R4aYCaYuK1y2HP0v4OHmTxVgjl4eq\nyM7Ckzl+3bdxG8vFbYUsQwaCYc75bZ0Wv2I05Qm0CE/n8rGSAMkJ1JJnGq6S9a6v\nlz1xmmFOR8Yt7knQFddzumoP8/IAsWBs/9WyKE6FiknRGuxZkwb/G0mmmUwyYKgP\nUhsaz3/VB0H0y3js+9JnBQgyFu6sXPOFKDrDGDcSgPyQJabiOIL0Uxju+Gel070l\n8A/IkJWlAgMBAAECggEAL3rSJ59Wh+EHy6G4bnB7FcNB1pXPIld/4AvASSkI55Vc\numW6t3bZoKzDqMoL8pjewdwFRGlqQgNA/VG0wM/yR6UExP//AvWMYVyzAXgHl4H1\npB27AilkuiHJRh0qMI/25C2EQQnFsVECyfLVZCm9lv1rm/qiJ8mCdhhxpB0WOpEb\nESyrVn7b4JHaQQUv1raj2q25ChF2s1XfDVGkoHvZezOQVfXeeESso9oHOv+uDv/q\nKgUfHoEus22zYTsTYaAN63BirXvUR9AH8ElprxnFYJDAxB0J04TbiVKj8TlcrdSX\nKw2zfeGVltpKTF6fXwqLvCs1V05XgX47EmRgQQGzcQKBgQD0WG7a1masHUtdI9FJ\nHLZob1glHCSy7mnvTB4BQQG7xDbu/AWmbTpykN8e/zAUavALONnHgWe2lGaJPHhO\nBoR36UuFFCD2YSXMTgyGG36RLBDrGxu/nrotFO+XrZeT8viOKKxscPt5XY+6kbzW\nZ5DK7CI8z+eWOg1iP+3MihnBZwKBgQDXjttAbSYabuWo/BDffRpDD5emX5GyKsyS\nCeKwisYcyY6l4OTW3/5zbyr/9goPTeJ9Ujj+fR7jj26FWuhHTyOw8n9L0ap56UK3\ndWrJmNEpQTL7bmh2mtrzSDq5ggm3FxO9dDvqSnFzj29PBCYgnbHlSDEa6S0jwWUM\n9QyTuWMNEwKBgFp54Fph51S16DEN0TH8UsKq7/2VxTUIOOulyLp4IrcvOZQWd9Uf\nPvQrFqM4pMn6qvXM0D8ieI/PtYP9gyKJIUXxechGIsp4QHG9OL/zsFL6+SzFNeon\nURLeXLVSTv02i7i/8NCaqVNQRKc1zbGqWZ/5/dd8uGkVB92ohFonjM/jAoGALH3h\nBVhCEByprIpylR6GSCc0vvNk6ChrVw1RS+0o3RJBLj3QHgXXyE2+y1wmUWhbAlDx\nUJ3UvVWCk2XjIazVChXTbnvVOxqJWcKTiw7OxTwe4MS2LRzjammTLW0IwlCyE12l\nbEN3TUniyKVrlTnXWSLgd9R/K60kRVtjAC64oh8CgYEA8RJuts1HrVo/qSqzoK0Q\nFuA5N5Hg3SL/mygS8kBVMtnecUtRpY6HFwY0Wg/tQRwzixSBVINLW9CDydcxUy/X\nOv+y7e56VPLJsk76VkzChTxPtNLiAxTqItNBbt+Si3w59NvfkkJMNalinN8S1gO5\ncaYcdNboL2jM2iv1U6eQwiM=\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-fbsvc@aolic-a3c42.iam.gserviceaccount.com",
-  client_id: "101596503546946949609",
+   type: "service_account",
+  project_id: "art-of-living-3d0d9",
+  private_key_id: "7ec2ce5ddc47ea49d06fdcfd24d67be846a5292b",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC+B0hA7Y22tKrk\n8Fo2opR/QcwEjIpEDjrvh/55plcKJX1P9bTIz8Z6mB+9q3cakqUzh8o/OrYHBclb\nDs9wR9ZhpQ3bAQn77Yo6VFqHAVs+xPJVXFhbbgXgP/EsEezMfuNWko7GvFlN/a/x\nrNVsE3u3wVBAW+00TqxuEytLkBwjiPCzJfwebH97aYu6dPjiZbfgq7Pmi60BVJ6b\niUk/iZaBaqbzL7kKBbujxXTnbjDm0R0EU/1Kh+HDqaxXW4uo1/Q0Z8tMrWNzJPKG\nSKYC751oTBEEX79Zw2zXZASWCCZbTwGPnO5uVRoQ80/qmaKalxAQwruua7fYlBZ/\nM4W82khzAgMBAAECgf8ZtSNOyPtFz81BzZaNpruAW1/m3G125+dYgFFrR0/jlF9m\nCiVA0Zb+lxrEYK7F8z4r1GbSiDV8Yv4+/xlr1l+Lfq7iyvnmS7RNmsESpjMC5mrY\nktyyN0Bm4iwsf5FPIrE9eaztxHNgoccrrSxV51AZcfOjt5ThHXXOmIh5/kKFvId+\nBYxaO0XeQYHx8u912ebkp3qVgizhkiWWL05cBG+5jMOfAh2bGmBD/KeV5yVDL2mk\nwnB2R2iZFduyp6KsOcIJoqncq8bVevp5JPuo+I67RcWPPRY99gNK4PfBEYFrqrUD\n7ipj6OfdzwqXbvfe/pB4u5MQbv0qJEd5o9JdzgECgYEA7YBLVp86yv5g6v0efqr1\n8biXRDBweaZEtR3ZXNXuC68qYpZLuJGqQU6l+eZZYw8wSF54u42fDn/Jul9vvqpq\ngQZGp0OhKlYmDK7v/93MQlSCVHeaFrVTWPaSJ2TuZGMrFt3ZiP1n9nWkG4zS82rm\n3AHvxA4e7hJqPFh3kyJzpLECgYEAzNRlS1bnX+o/ZN4sac+QXvhd2aVzh34/hK1j\nVgzsrUnubjvEE4Jv0nYuwqfQVeIYeNBfgnB9Pl+pfEW4NtRp5FJtLJ2vpHIE9qbn\n5XAlHaaidgZtO0PNodE/GRMUMFHoNn50fdTcQtP4NqWubfbjcOTcu199/Hax5Kfk\ns04BGGMCgYEA0VbyEQoM4nxRpQ8y3+eFHWjdPCeOHEvn59TFkMSoK/hb3cYoPCl4\n7lOKjLjzNRNgNYm/1WFBP6B3iSUx89d2RP/pKum7lvuUUj2oIdm4fwbKUfWSBk4R\nhyrfLIYUcrvOcTte0Ra7mkubOQVps1EY5/d1BVSFAseMiGBaFYCaeiECgYBqG4U1\n2BkYKOMaP7XGQo5JhGQpwXntsMr60k9pXlylEG2BkVhruuBuqGZk1EYql2uR+m/f\nxKI805WbGQrq7HLGoqSHvlAk4iQMGj57LvMrPw1eEso3L1KxBxKmzKlitquSv0Nd\nhA1uWRx2QrHvqTv9WbG3Jyq+xEY3a3ogIMKUfQKBgQCx1XGtKb2Gn+/DM+NG7ofo\nUplgrzqnrX0uf24JVEOllwJtHpoEdxfWgk32Jzos6zK/1S6cpXPUNPhFANPpPIH1\n30JiO/llsESr7jg60N3Eb46zR5V/kp46HNw5lj3USCnwA3dam98JoAGUI8kljiax\nJSe2+F+p3nvMfBVowjKF8Q==\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-fbsvc@art-of-living-3d0d9.iam.gserviceaccount.com",
+  client_id: "101017867378810317609",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aolic-a3c42.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40art-of-living-3d0d9.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
 };
+
+
